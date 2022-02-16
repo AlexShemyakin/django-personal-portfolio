b94e7bd03d3b95075f950eb285e38d08e1a2e8ed
AlexShemyakin <fax777@rambler.ru>
2022-02-16 20:37:10 +0300

    Edit settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mid+68t-!(a22qwgb(21i6qs&blac-_u(*q(t(kout7ti=w+^y'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -134,4 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 try:
     from .local_settings import *
 except ImportError:
-    print("Looks like no local file. You must on production")
+    print("Looks like no local file.")
